5269d5a8ea43e0a8f0bb572019dadf582cacddbe
chen-singh <cssodho123@gmail.com>
2026-07-17 21:34:23 +0400

    add files

--- WhatsAppOrderManagementSystem/src/main/java/in/cs/main/controller/AuthController.java
+++ WhatsAppOrderManagementSystem/src/main/java/in/cs/main/controller/AuthController.java
@@ -1,5 +1,8 @@
 package in.cs.main.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class AuthController {
 
 }
--- WhatsAppOrderManagementSystem/src/main/java/in/cs/main/service/DelieveryService.java
+++ WhatsAppOrderManagementSystem/src/main/java/in/cs/main/service/DelieveryService.java
@@ -1,5 +1,8 @@
 package in.cs.main.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class DelieveryService {
 
 }
