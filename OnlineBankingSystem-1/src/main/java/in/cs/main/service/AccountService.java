9630efb1844f225fe5d0fd7c8ff60cac9bdb080f
chen-singh <cssodho123@gmail.com>
2026-09-07 20:32:06 +0400

    add files

--- OnlineBankingSystem-1/src/main/java/in/cs/main/service/AccountService.java
+++ OnlineBankingSystem-1/src/main/java/in/cs/main/service/AccountService.java
@@ -1,8 +1,21 @@
 package in.cs.main.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import in.cs.main.entities.Accounts;
+import in.cs.main.repository.AccountRepository;
 
 @Service
 public class AccountService {
 
+	@Autowired
+	private AccountRepository accountrepo;
+	
+	private Accounts createAccount(Accounts account) {
+		return accountrepo.save(account);
+		
+	}
+	
+	
 }
--- OnlineBankingSystem-1/src/main/java/in/cs/main/service/UserService.java
+++ OnlineBankingSystem-1/src/main/java/in/cs/main/service/UserService.java
@@ -1,5 +1,42 @@
 package in.cs.main.service;
 
-public class UserService {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import in.cs.main.entities.Users;
+import in.cs.main.repository.UserRepository;
+
+@Service
+public class UserService {
+	
+    @Autowired
+	private UserRepository userrepo;
+	
+    
+    public Users createUser(Users user) {
+    	
+    	if (userrepo.existsByUsername(user.getUsername())) {
+            throw new RuntimeException("Username already exists");
+        }
+
+    	
+        user.setRole("CUSTOMER");
+		return userrepo.save(user);
+    	
+    }
+    public Users findByUsername(String username) {
+
+        return userrepo.findByUsername(username)
+                .orElseThrow(() ->
+                        new RuntimeException("User not found"));
+    }
+
+    // Find user by ID
+    public Users findById(Integer userId) {
+
+        return userrepo.findById(userId)
+                .orElseThrow(() ->
+                        new RuntimeException("User not found"));
+    }
+    
 }
