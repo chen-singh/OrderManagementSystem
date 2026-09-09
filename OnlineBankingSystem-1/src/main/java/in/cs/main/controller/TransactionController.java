fe661fe482895e9e2be7f88b14873420ae3cc1d7
chen-singh <cssodho123@gmail.com>
2026-09-09 21:15:00 +0400

    add files

---
+++ OnlineBankingSystem-1/src/main/java/in/cs/main/controller/TransactionController.java
@@ -0,0 +1,99 @@
+package in.cs.main.controller;
+
+
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import in.cs.main.entities.Transaction;
+import in.cs.main.service.TransactionService;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/transactions")
+@CrossOrigin(origins = "*")
+public class TransactionController {
+
+    private final TransactionService transactionService;
+
+    public TransactionController(
+            TransactionService transactionService) {
+
+        this.transactionService = transactionService;
+    }
+
+   
+    // DEPOSIT
+   
+
+    @PostMapping("/deposit")
+    public ResponseEntity<Transaction> deposit(
+            @RequestParam String accountNumber,
+            @RequestParam BigDecimal amount) {
+
+        Transaction transaction =
+                transactionService.deposit(
+                        accountNumber,
+                        amount
+                );
+
+        return ResponseEntity.ok(transaction);
+    }
+
+
+    // WITHDRAW
+   
+
+    @PostMapping("/withdraw")
+    public ResponseEntity<Transaction> withdraw(
+            @RequestParam String accountNumber,
+            @RequestParam BigDecimal amount) {
+
+        Transaction transaction =
+                transactionService.withdraw(
+                        accountNumber,
+                        amount
+                );
+
+        return ResponseEntity.ok(transaction);
+    }
+
+
+    // TRANSFER
+    
+
+    @PostMapping("/transfer")
+    public ResponseEntity<Transaction> transfer(
+            @RequestParam String fromAccount,
+            @RequestParam String toAccount,
+            @RequestParam BigDecimal amount) {
+
+        Transaction transaction =
+                transactionService.transfer(
+                        fromAccount,
+                        toAccount,
+                        amount
+                );
+
+        return ResponseEntity.ok(transaction);
+    }
+
+
+    // TRANSACTION HISTORY
+  
+
+    @GetMapping("/history/{accountId}")
+    public ResponseEntity<List<Transaction>> getHistory(
+            @PathVariable Integer accountId) {
+
+        List<Transaction> transactions =
+                transactionService.getTransactionHistory(
+                        accountId
+                );
+
+        return ResponseEntity.ok(transactions);
+    }
+}
+
